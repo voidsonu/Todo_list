f4faee1953ad7c973e466f16e02f3614bbe9de12
voidsonu <sonudragneel4@gmail.com>
2022-01-23 17:30:46 +0530

    fix complete todo list

--- src/components/Todoslist.js
+++ src/components/Todoslist.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 export const Todoslist = ({todos, setTodos, setEditTodo})=>{
-    const handleComplete = ({todo})=>{
+    const handleComplete = ({id})=>{
+        // debugger
         setTodos(
             todos.map((item)=>{
-                if(item.id === todo.id){
+                if(item.id === id){
                     return{...item, completed : !item.completed}
                 }
                 return item; 
             })
-        );
+            );
+            console.log(todos);
     };
     const handleEdit = ({id})=>{
         const findTodo = todos.find((todo)=> todo.id === id);
@@ -22,7 +24,7 @@ export const Todoslist = ({todos, setTodos, setEditTodo})=>{
         <div>
             {todos.map((todo)=>(
                 <li className='list-item' key={todo.id}> 
-                <input type="text" value={todo.title} className={'list ${todo.completed ? "complete" : ""}'} onChange={(event)=> event.preventDefault()} />
+                <input type="text" value={todo.title} className={"list" + (todo.completed ? " complete" : "")} onChange={(event)=> event.preventDefault()} />
                 <div>
                     <button className="button-complete task-button" onClick={()=>handleComplete(todo)}>
                         <i className="fa fa-check-circle"></i>
